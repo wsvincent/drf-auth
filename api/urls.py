bdf394d8a5eef865fb7c53b9d240cedd65647ff4
wsvincent <will@wsvincent.com>
2018-03-13 17:03:20 -0400

    initial commit

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import include, path
+
+from . import views
+
+urlpatterns = [
+    path('', views.ListTodo.as_view()),
+    path('<int:pk>/', views.DetailTodo.as_view()),
+    path('rest-auth/', include('rest_auth.urls')),
+]
